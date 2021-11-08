351fe5b92917bb7fc1ea11361bbd03edfe3932c9
TheMipmap <morten-ks@hotmail.com>
2021-11-08 15:28:02 +0100

    Udgangspunktet til konkurrencen i RP

---
+++ RP_konkurrence.ino
@@ -0,0 +1,18 @@
+#include<Wire.h>
+#include<Zumo32U4.h> //Include the needed libraries
+
+Zumo32U4Motors motors;
+Zumo32U4Encoders encoders;    //initialize the different Zumo-functionalities
+Zumo32U4ButtonA buttonA;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);  // Start the serial-communication between the robot and the computer.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
