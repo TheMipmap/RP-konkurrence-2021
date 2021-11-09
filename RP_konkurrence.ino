6a820a98c9ae28508b5f9273bb0731ddb3a2ce72
TheMipmap <morten-ks@hotmail.com>
2021-11-09 12:16:30 +0100

    Kopirer den friske start over til branchen for den første milestone

--- RP_konkurrence.ino
+++ RP_konkurrence.ino
@@ -10,9 +10,11 @@ Zumo32U4ButtonA buttonA;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);  // Start the serial-communication between the robot and the computer.
+  buttonA.waitForPress(); //Starts the program after a click on button A and a small delay
+  delay(100);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+   
 }
